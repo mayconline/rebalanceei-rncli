0de9851d7d16541f5f4ab6be7a9682d1c0f10cf2
mayconline <mayconline.ti@gmail.com>
2021-01-07 18:42:30 -0300

    change termsuse

--- src/utils/Terms.ts
+++ src/utils/Terms.ts
@@ -2,5 +2,5 @@ import { Linking } from 'react-native';
 
 export const getTerms = () =>
   Linking.openURL(
-    'https://res.cloudinary.com/apinodeteste/image/upload/v1604512771/Rebalanceei/TermsOfServices_and_PrivacyPolicy_k8e0r5.pdf',
+    'https://res.cloudinary.com/apinodeteste/image/upload/v1610055701/Rebalanceei/TermsOfUseAndPrivacyPolicies_l6a3bo.pdf',
   );
