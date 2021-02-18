e499834ebd1cc185bd434d952e86e8f0dc1c0fd7
mayconline <mayconline.ti@gmail.com>
2021-02-18 19:06:48 -0300

    open plan modal when limited wallet or tickett

--- src/components/TextError/index.tsx
+++ src/components/TextError/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'react-native';
+import PlanModal from '../../modals/PlanModal';
 import { formatErrors } from '../../utils/format';
 import { TextContentError } from './styles';
 
@@ -7,10 +9,31 @@ interface ITextError {
   isTabs?: boolean;
 }
 
-const TextError = ({ children, isTabs }: ITextError) => (
-  <TextContentError isTabs={isTabs} numberOfLines={1} ellipsizeMode="tail">
-    {formatErrors(children)}
-  </TextContentError>
-);
+const TextError = ({ children, isTabs }: ITextError) => {
+  const openPlanModal =
+    children === 'Tickets limited to 16 items' ||
+    children === 'Wallet limited to 2 items';
+
+  const [openModal, setOpenModal] = useState(openPlanModal);
+
+  return (
+    <>
+      <TextContentError isTabs={isTabs} numberOfLines={1} ellipsizeMode="tail">
+        {formatErrors(children)}
+      </TextContentError>
+
+      {openModal && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={openModal}
+          statusBarTranslucent={true}
+        >
+          <PlanModal onClose={() => setOpenModal(false)} />
+        </Modal>
+      )}
+    </>
+  );
+};
 
 export default TextError;
