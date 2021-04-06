ce2b40d104cb0c2697c3107462c663f8e9246b80
mayconline <mayconline.ti@gmail.com>
2021-04-06 00:23:39 -0300

    add prod api key on amplitude

--- src/hooks/useAmplitude.tsx
+++ src/hooks/useAmplitude.tsx
@@ -1,9 +1,10 @@
 import { Amplitude } from '@amplitude/react-native';
 
 const DEV_KEY = '57549736bd55c979fad08a8e10e140a0';
+const DEV_PROD = 'ee843415251026d68d635fc4adca0aa8';
 
 const ampInstance = Amplitude.getInstance();
-ampInstance.init(DEV_KEY);
+ampInstance.init(DEV_PROD);
 
 const useAmplitude = () => {
   const logEvent = (event: string) => {
