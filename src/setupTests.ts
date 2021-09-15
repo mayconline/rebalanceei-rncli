f7e96582b501fbf5e654ffaea1180dc06eec077f
mayconline <mayconline.ti@gmail.com>
2021-09-14 23:24:26 -0300

    mock adMob for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,17 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 
-jest.mock('react-native-admob', () => () => null);
+jest.mock('./services/AdMob', () => ({
+  initializeAdMob: () => null,
+  useInterstitialAd: () => ({
+    adLoaded: false,
+    show: false,
+    adShowing: false,
+    adDismissed: false,
+  }),
+  BannerAd: () => null,
+}));
+
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-reanimated', () => {
