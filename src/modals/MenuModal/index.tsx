58f3e6d11ba9d2a2afadb007b78f19be77c64b62
mayconline <mayconline.ti@gmail.com>
2023-01-12 22:44:39 -0300

    change version 1.7.10

--- src/modals/MenuModal/index.tsx
+++ src/modals/MenuModal/index.tsx
@@ -52,7 +52,7 @@ const menuItens = [
   {
     lib: Octicons,
     icon: 'versions',
-    description: 'Versão do APP - v1.7.9',
+    description: 'Versão do APP - v1.7.10',
   },
   {
     lib: AntDesign,
@@ -92,7 +92,7 @@ const MenuModal = ({ onClose }: MenuProps) => {
         return getTerms();
       case 'Ajuda':
         return setOpenModal('Help');
-      case 'Versão do APP - v1.7.9':
+      case 'Versão do APP - v1.7.10':
         return;
       case 'Sair':
         return handleSignOut();
