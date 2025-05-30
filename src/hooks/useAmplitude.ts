46d94aa1c67d272bbf318237a171c93bc994fa8a
mayconline <mayconline.ti@gmail.com>
2025-05-29 22:34:28 -0300

    fix: useLogEvent

--- src/hooks/useAmplitude.ts
+++ src/hooks/useAmplitude.ts
@@ -1,18 +1,18 @@
 import { Amplitude } from '@amplitude/react-native';
-
+import { useCallback } from 'react';
 import Config from '../config/envs';
 
 const ampInstance = Amplitude.getInstance();
 ampInstance.init(Config?.amplitudeKey);
 
 const useAmplitude = () => {
-  const logEvent = (event: string) => {
+  const logEvent = useCallback((event: string) => {
     try {
       return ampInstance.logEvent(event);
     } catch (err: any) {
       console.log(err);
     }
-  };
+  }, []);
 
   return {
     logEvent,
